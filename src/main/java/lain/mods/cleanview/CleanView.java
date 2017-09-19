8aa9ddfea9dcdcbde9e2adf4b31e961ec53069c7
Lain MultipleInstances <zlainsama@163.com>
2017-09-20 00:48:44 +0800

    1.12.2

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -3,7 +3,7 @@ package lain.mods.cleanview;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "cleanview", useMetadata = true, acceptedMinecraftVersions = "[1.10,)")
+@Mod(modid = "cleanview", useMetadata = true, acceptedMinecraftVersions = "[1.12,)", certificateFingerprint = "aaaf83332a11df02406e9f266b1b65c1306f0f76")
 public class CleanView
 {
 
