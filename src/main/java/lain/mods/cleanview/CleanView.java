f6f29cd803729426685179f5e851c173422fcf86
Lain MultipleInstances <zlainsama@163.com>
2020-11-03 15:33:26 +0800

    update to 1.16.4 fabric
    remove windows scripts
    switch to IDEA
    reformat all
    update .gitignore

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -1,9 +1,5 @@
 package lain.mods.cleanview;
 
-import java.lang.ref.WeakReference;
-import java.lang.reflect.Field;
-import java.util.Collection;
-import org.lwjgl.glfw.GLFW;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -16,9 +12,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.potion.PotionUtil;
+import org.lwjgl.glfw.GLFW;
 
-public class CleanView implements ClientModInitializer
-{
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+public class CleanView implements ClientModInitializer {
 
     private KeyBinding keyToggle;
     private boolean lastState;
@@ -26,18 +26,15 @@ public class CleanView implements ClientModInitializer
     private TrackedData<Integer> colors;
     private WeakReference<Entity> lastCam;
 
-    private void onClientTick(MinecraftClient client)
-    {
+    private void onClientTick(MinecraftClient client) {
         Entity ent = client.getCameraEntity();
         Entity prevEnt = lastCam.get();
 
         if (disabled)
             ent = null;
 
-        if (prevEnt != ent)
-        {
-            if (prevEnt instanceof LivingEntity)
-            {
+        if (prevEnt != ent) {
+            if (prevEnt instanceof LivingEntity) {
                 Collection<StatusEffectInstance> effects = ((LivingEntity) prevEnt).getStatusEffects();
                 if (!effects.isEmpty())
                     prevEnt.getDataTracker().set(colors, PotionUtil.getColor(effects));
@@ -50,29 +47,23 @@ public class CleanView implements ClientModInitializer
     }
 
     @Override
-    public void onInitializeClient()
-    {
+    public void onInitializeClient() {
         setupKeyBindings();
         setupEvents();
     }
 
-    private void onKeyBindingActivate(KeyBinding keybinding)
-    {
+    private void onKeyBindingActivate(KeyBinding keybinding) {
         if (keybinding == keyToggle)
             disabled = !disabled;
     }
 
     @SuppressWarnings("unchecked")
-    private void setupEvents()
-    {
-        try
-        {
+    private void setupEvents() {
+        try {
             Field f = LivingEntity.class.getDeclaredField(FabricLoader.getInstance().getMappingResolver().mapFieldName("intermediary", "net.minecraft.class_1309", "field_6240", "Lnet/minecraft/class_2940;"));
             f.setAccessible(true);
             colors = (TrackedData<Integer>) f.get(null);
-        }
-        catch (Throwable t)
-        {
+        } catch (Throwable t) {
             throw new IllegalStateException("[CleanView] Failed to acquire specific field for the mod.", t);
         }
 
@@ -83,27 +74,20 @@ public class CleanView implements ClientModInitializer
         });
     }
 
-    private void setupKeyBindings()
-    {
+    private void setupKeyBindings() {
         keyToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.cleanview.togglecleanview", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.misc"));
     }
 
-    private void tickKeyBindings()
-    {
-        if (keyToggle != null)
-        {
-            try
-            {
+    private void tickKeyBindings() {
+        if (keyToggle != null) {
+            try {
                 boolean state = keyToggle.isPressed();
-                if (lastState != state)
-                {
+                if (lastState != state) {
                     if (state)
                         onKeyBindingActivate(keyToggle);
                     lastState = state;
                 }
-            }
-            catch (Throwable t)
-            {
+            } catch (Throwable t) {
                 keyToggle = null;
                 t.printStackTrace();
                 System.err.println("[CleanView] Failed to check KeyBinding state, the toggle key will not work.");
