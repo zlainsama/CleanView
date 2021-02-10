756011f649cf4591f953d9e5e34d996f75c21a1b
Lain MultipleInstances <zlainsama@163.com>
2021-02-10 20:19:19 +0800

    register DISPLAYTEST as client-only

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -1,13 +1,18 @@
 package lain.mods.cleanview;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("cleanview")
 public class CleanView {
 
     public CleanView() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "client-only", (v, n) -> n));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
     }
 
