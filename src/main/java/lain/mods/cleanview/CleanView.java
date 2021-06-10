0b601fc0637c763b0d283f37115e6e4562911863
Lain MultipleInstances <zlainsama@163.com>
2021-06-10 21:17:22 +0800

    update to 1.17 fabric
    switch to java 16

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
