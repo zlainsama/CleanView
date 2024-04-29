c2339577683fa6f6302c6d11b93ca1d11d14eaff
Lain MultipleInstances <zlainsama@live.com>
2024-04-29 17:54:29 +0800

    update to 1.20.5 neoforge

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -1,18 +1,18 @@
 package lain.mods.cleanview;
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 @Mod("cleanview")
 public class CleanView {
 
-    public CleanView() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+    public CleanView(IEventBus bus) {
+        bus.addListener(this::setupClient);
         if (FMLEnvironment.dist.isClient()) {
-            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupKeyMappings);
+            bus.addListener(this::setupKeyMappings);
         }
     }
 
--- src/main/java/lain/mods/cleanview/Proxy.java
+++ src/main/java/lain/mods/cleanview/Proxy.java
@@ -3,18 +3,18 @@ package lain.mods.cleanview;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.alchemy.PotionUtils;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.TickEvent;
 import org.lwjgl.glfw.GLFW;
 
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
-import java.util.Collection;
+import java.util.List;
 import java.util.function.Consumer;
 
 enum Proxy {
@@ -24,7 +24,7 @@ enum Proxy {
     KeyMapping keyToggle = new KeyMapping("key.togglecleanview", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.misc");
     boolean lastState = false;
     boolean enabled = true;
-    EntityDataAccessor<Integer> potionEffects = null;
+    EntityDataAccessor<List<ParticleOptions>> effectParticles = null;
     WeakReference<Entity> lastCam = new WeakReference<>(null);
 
     void handleClientTickEvent(TickEvent.ClientTickEvent event) {
@@ -44,15 +44,15 @@ enum Proxy {
 
             if (prevEnt != ent) {
                 if (prevEnt instanceof LivingEntity) {
-                    Collection<MobEffectInstance> effects = ((LivingEntity) prevEnt).getActiveEffects();
-                    if (!effects.isEmpty())
-                        prevEnt.getEntityData().set(potionEffects, PotionUtils.getColor(effects));
+                    List<ParticleOptions> particles = ((LivingEntity) prevEnt).getActiveEffectsMap().values().stream().filter(MobEffectInstance::isVisible).map(MobEffectInstance::getParticleOptions).toList();
+                    if (!particles.isEmpty())
+                        prevEnt.getEntityData().set(effectParticles, particles);
                 }
                 lastCam = new WeakReference<>(ent);
             }
 
             if (ent instanceof LivingEntity)
-                ent.getEntityData().set(potionEffects, 0);
+                ent.getEntityData().set(effectParticles, List.of());
         }
     }
 
@@ -67,11 +67,11 @@ enum Proxy {
         Throwable t = null;
         Field f;
         try {
-            f = LivingEntity.class.getDeclaredField("f_20962_");
+            f = LivingEntity.class.getDeclaredField("DATA_EFFECT_PARTICLES");
         } catch (Throwable t1) {
             t = t1;
             try {
-                f = LivingEntity.class.getDeclaredField("DATA_EFFECT_COLOR_ID");
+                f = LivingEntity.class.getDeclaredField("bL");
             } catch (Throwable t2) {
                 t.addSuppressed(t2);
                 f = null;
@@ -80,7 +80,7 @@ enum Proxy {
         try {
             if (f != null) {
                 f.setAccessible(true);
-                potionEffects = (EntityDataAccessor<Integer>) f.get(null);
+                effectParticles = (EntityDataAccessor<List<ParticleOptions>>) f.get(null);
             }
         } catch (Throwable t3) {
             if (t == null)
@@ -88,7 +88,7 @@ enum Proxy {
             else
                 t.addSuppressed(t3);
         }
-        if (potionEffects == null)
+        if (effectParticles == null)
             throw new IllegalStateException("[CleanView] Failed to acquire specific field for the mod.", t);
     }
 
