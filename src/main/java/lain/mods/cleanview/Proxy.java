1258ab29189233c6999319483d63397ff3ff29d1
Lain MultipleInstances <zlainsama@live.com>
2024-06-10 22:17:27 +0800

    update to 1.20.6

--- src/main/java/lain/mods/cleanview/Proxy.java
+++ src/main/java/lain/mods/cleanview/Proxy.java
@@ -3,18 +3,18 @@ package lain.mods.cleanview;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import org.lwjgl.glfw.GLFW;
 
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
-import java.util.Collection;
+import java.util.List;
 import java.util.function.Consumer;
 
 enum Proxy {
@@ -24,7 +24,7 @@ enum Proxy {
     KeyMapping keyToggle = new KeyMapping("key.togglecleanview", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.misc");
     boolean lastState = false;
     boolean enabled = true;
-    EntityDataAccessor<Integer> potionEffects = null;
+    EntityDataAccessor<List<ParticleOptions>> potionEffects = null;
     WeakReference<Entity> lastCam = new WeakReference<>(null);
 
     void handleClientTickEvent(TickEvent.ClientTickEvent event) {
@@ -44,15 +44,15 @@ enum Proxy {
 
             if (prevEnt != ent) {
                 if (prevEnt instanceof LivingEntity) {
-                    Collection<MobEffectInstance> effects = ((LivingEntity) prevEnt).getActiveEffects();
-                    if (!effects.isEmpty())
-                        prevEnt.getEntityData().set(potionEffects, PotionUtils.getColor(effects));
+                    List<ParticleOptions> list = ((LivingEntity) prevEnt).getActiveEffects().stream().filter(MobEffectInstance::isVisible).map(MobEffectInstance::getParticleOptions).toList();
+                    if (!list.isEmpty())
+                        prevEnt.getEntityData().set(potionEffects, list);
                 }
                 lastCam = new WeakReference<>(ent);
             }
 
             if (ent instanceof LivingEntity)
-                ent.getEntityData().set(potionEffects, 0);
+                ent.getEntityData().set(potionEffects, List.of());
         }
     }
 
@@ -67,20 +67,25 @@ enum Proxy {
         Throwable t = null;
         Field f;
         try {
-            f = LivingEntity.class.getDeclaredField("f_20962_");
+            f = LivingEntity.class.getDeclaredField("DATA_EFFECT_PARTICLES");
         } catch (Throwable t1) {
             t = t1;
             try {
-                f = LivingEntity.class.getDeclaredField("DATA_EFFECT_COLOR_ID");
+                f = LivingEntity.class.getDeclaredField("f_314048_");
             } catch (Throwable t2) {
                 t.addSuppressed(t2);
-                f = null;
+                try {
+                    f = LivingEntity.class.getDeclaredField("bL");
+                } catch (Throwable t3) {
+                    t.addSuppressed(t3);
+                    f = null;
+                }
             }
         }
         try {
             if (f != null) {
                 f.setAccessible(true);
-                potionEffects = (EntityDataAccessor<Integer>) f.get(null);
+                potionEffects = (EntityDataAccessor<List<ParticleOptions>>) f.get(null);
             }
         } catch (Throwable t3) {
             if (t == null)
