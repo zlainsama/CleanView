112a68e5bfc6224d6b17fe329a3e90daca162746
Lain MultipleInstances <zlainsama@live.com>
2025-04-27 01:52:36 +0800

    update to 1.21.5

--- src/main/java/lain/mods/cleanview/CleanView.java
+++ src/main/java/lain/mods/cleanview/CleanView.java
@@ -1,20 +1,19 @@
 package lain.mods.cleanview;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod("cleanview")
 public class CleanView {
 
     public CleanView(FMLJavaModLoadingContext context) {
         context.getModEventBus().addListener(this::setupClient);
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+        if (FMLEnvironment.dist.isClient()) {
             context.getModEventBus().addListener(this::setupKeyMappings);
-        });
+        }
     }
 
     private void setupClient(FMLClientSetupEvent event) {
